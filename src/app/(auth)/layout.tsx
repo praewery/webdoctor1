3856b1cc372779edc2c736933688ef37f347cb20
praewa choobanna <cytokinin8@gmail.com>
2023-10-09 02:46:58 +0700

    first commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+import '../globals.css'
+export const matadata = {
+    title: 'Onboarding',
+    description: 'Onboarding',
+}
+
+const inter = Inter({ subsets: ['latin']})
+
+export default function RootLayer({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+       <ClerkProvider>
+        <html lang ="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+
+       </ClerkProvider>
+    )   
+}
